296aae6913bd273b14895d27d52860468abb9783
Andreas Tarandi <torandi@gmail.com>
2013-03-24 00:18:45 +0100

    Better sample

--- nxauth.sample.php
+++ nxauth.sample.php
@@ -6,8 +6,10 @@ require_once "libs/nxauth/include.php";
 
 	/* Settings for CAS via NitroXy.com */
 
+	$dir = dirname(__FILE__); /* path to cert must be absolute, so include this */
+
 	$cas_config = array(
-		'site' => "nitroxy.torandi.com",
+		'site' => "nitroxy.com",
 		'port' => 443,
 		'key_id' => "nxauth", /* The id for the local site, used thougheter with the private key for extra data */
 		'private_key' => null, /* Specify path to applications private key (NEVER check in the private key to a git repo) for extra data (if any) */
