4c179462a7c90202ba37c139f3e9465bf854401f
Andreas Tarandi <torandi@gmail.com>
2013-03-24 00:19:00 +0100

    Include root in include

--- include.php
+++ include.php
@@ -2,7 +2,7 @@
 
 $nxauth_root = dirname(__FILE__);
 
-require_once "$nxauth_root/libs/phpCAS/CAS.php";
-require_once "models/NXAuth.php";
-require_once "models/NXUser.php";
-require_once "models/NXAPI.php";
+require_once "$nxauth_root/phpCAS/CAS.php";
+require_once "$nxauth_root/models/NXAuth.php";
+require_once "$nxauth_root/models/NXUser.php";
+require_once "$nxauth_root/models/NXAPI.php";
