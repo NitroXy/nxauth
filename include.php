c0dd7cb002b052e00efc59f6aeb60cc3cbf73d82
David Sveningsson <ext@sidvind.com>
2016-04-08 23:37:37 +0200

    pull phpcas via composer instead of bundle

--- include.php
+++ include.php
@@ -2,7 +2,7 @@
 
 $nxauth_root = dirname(__FILE__);
 
-require_once "$nxauth_root/phpCAS/CAS.php";
+require_once "$nxauth_root/vendor/autoload.php";
 require_once "$nxauth_root/models/NXAuth.php";
 require_once "$nxauth_root/models/NXUser.php";
 require_once "$nxauth_root/models/NXAPI.php";
